938aee97df09755ab16a0b57ba74bba95e85faa1
kosolal1 <leo.kosola@aalto.fi>
2019-04-28 17:28:57 +0300

    Updates

---
+++ precache-manifest.f012579476c8bba6abc510f0a852e68a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a67a72763f4854cd824f0e8488b19d9",
+    "url": "/kaltaisesi/index.html"
+  },
+  {
+    "revision": "8f78f36267de70787d3e",
+    "url": "/kaltaisesi/static/css/main.66ce6b4f.chunk.css"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/kaltaisesi/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "8f78f36267de70787d3e",
+    "url": "/kaltaisesi/static/js/main.c967b70b.chunk.js"
+  },
+  {
+    "revision": "afc66e028d5afcbcad8a",
+    "url": "/kaltaisesi/static/js/runtime~main.3ac15e84.js"
+  }
+]);
