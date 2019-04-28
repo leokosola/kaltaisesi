524506085eaaab59c2936933b90d076c5df842db
kosolal1 <leo.kosola@aalto.fi>
2019-04-28 22:38:33 +0300

    last push: comments, naming, more readable code etc

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
 const App = () => {
-  const data = require('./data.json')
+  const data = require('./data.json') // Käytetty data-aineisto
 
-  const [filterString, setFilterString] = useState("")
-  const [education, setEducation] = useState(0)
-
+  // Hookit eli tässä tapauksessa boolean-tilat eri vastausvaihtoehdoille.
+  // Oletuksena false joka ei suodata mitään.
+  // Pois lukien nimihaun tila, joka on tyhjä string.
+  const [candidateName, setCandidateName] = useState("")
   const [male, setMale] = useState(false)
   const [female, setFemale] = useState(false)
   const [young, setYoung] = useState(false)
@@ -22,21 +21,14 @@ const App = () => {
   const [lukio, setLukio] = useState(false)
   const [korkeakoulu, setKorkeakoulu] = useState(false)
 
-  // Suodatetaan data-aineistoa muuttujaan
+  // Suodatetaan dataa lukijan valitsemilla muuttujilla.
+  // Jos true, suodatetaan valinnan mukaan. Jos false, ei suodateta mitään.
+  // Oletuksena siis näkyy kaikki nimet.
+  // Ekana tekstihaku nimille, sitten booleanit: sukupuoli, ikä, kieli ja koulutus.
   const filteredCandidates = data
-  // Jos lukija kirjoittaa nimensä, etsi datasta tyypit, joiden nimestä löytyy syötetty merkkijono
   .filter(d => 
-    d.firstName.toLowerCase().includes(filterString.toLowerCase()) || 
-    d.lastName.toLowerCase().includes(filterString.toLowerCase()))
-  .filter (d => {
-    if (education === 0) {
-      return d.education !== education
-      } 
-      else {
-        return d.education === education
-      }
-  })
-  // Jos lukija klikkaa nappia x, suodata datasta tähän valintaan sopivat tyypit
+    d.firstName.toLowerCase().includes(candidateName.toLowerCase()) || 
+    d.lastName.toLowerCase().includes(candidateName.toLowerCase()))
   .filter (d => male ? d.gender === 1 : d.gender)
   .filter (d => female ? d.gender === 2 : d.gender)
   .filter (d => young ? d.age < 35 : d.age)
@@ -49,10 +41,11 @@ const App = () => {
   .filter (d => lukio ? d.education === "ylioppilas" : d.education)
   .filter (d => korkeakoulu ? d.education === "korkeakoulututkinto" : d.education)
 
-  //Nollaa lukijan tekemät valinnat -> kaikki tilat falseiksi
+  //Funktio nollaamaan lukijan tekemät valinnat
+  // -> kaikki tilat falseiksi ja nimi tyhjäksi stringiksi
   const reset = () => {
     return () => {
-        setFilterString("")
+        setCandidateName("")
         setMale(false)
         setFemale(false)
         setYoung(false)
@@ -70,58 +63,100 @@ const App = () => {
   // Sivulle renderöityvät elementit
   return (
     <div className="wrapper">
-      <div className="aloitus">
+
+      <div className="beginning">
         <h1>Kaltaisesi</h1>
-        <p className="lead">Tähän ingressi</p>
+        <p className="lead">Kuinka moni vastavalitun eduskunnan 200 kansanedustajasta muistuttaa sinua? Vastaa kysymyksiin ja katso tulos.</p>
       </div>
+      <hr/>
+
       <div className="interactive">
-        <div className="valinnat">
+
+        <div className="selections">
           <h2>Kerro itsestäsi</h2>
+          <p>Vastaa niin moneen kohtaan kuin haluat. Jos et vastaa yhteenkään, voit tarkastella kaikkia uusia kansanedustajia <a className="ankkuri" href="#tulos">oheisesta listasta</a>. Emme tallenna vastauksia tai kerää sinusta dataa.</p>
+
           <h3>Sukupuoli</h3>
-          <Button class={male ? 'aktiivinen' : 'passiivinen' } suodatin={() => setMale(!male)} buttonText={'mies'} />
-          <Button class={female ? 'aktiivinen' : 'passiivinen' } suodatin={() => setFemale(!female)} buttonText={'nainen'} />
+          <Button class={male ? 'active' : 'passive'} 
+                  filter={() => setMale(!male)} 
+                  buttonText={'mies'} />
+          <Button class={female ? 'active' : 'passive'} 
+                  filter={() => setFemale(!female)} 
+                  buttonText={'nainen'} />
 
           <h3>Ikä</h3>
-          <Button class={young ? 'aktiivinen' : 'passiivinen' } suodatin={() => setYoung(!young)} buttonText={'alle 35'} />
-          <Button class={middleAge ? 'aktiivinen' : 'passiivinen' } suodatin={() => setMiddleAge(!middleAge)} buttonText={'35 - 50'} />
-          <Button class={old ? 'aktiivinen' : 'passiivinen' } suodatin={() => setOld(!old)} buttonText={'yli 50'} />
+          <Button class={young ? 'active' : 'passive'} 
+                  filter={() => setYoung(!young)} 
+                  buttonText={'alle 35'} />
+          <Button class={middleAge ? 'active' : 'passive'} 
+                  filter={() => setMiddleAge(!middleAge)} 
+                  buttonText={'35 - 50'} />
+          <Button class={old ? 'active' : 'passive'} 
+                  filter={() => setOld(!old)} 
+                  buttonText={'yli 50'} />
 
           <h3>Kieli</h3>
-          <Button class={fin ? 'aktiivinen' : 'passiivinen'} suodatin={() => setFin(!fin)} buttonText={'suomi'} />
-          <Button class={sve ? 'aktiivinen' : 'passiivinen'} suodatin={() => setSve(!sve)} buttonText={'ruotsi'} />
+          <Button class={fin ? 'active' : 'passive'} 
+                  filter={() => setFin(!fin)} 
+                  buttonText={'suomi'} />
+          <Button class={sve ? 'active' : 'passive'} 
+                  filter={() => setSve(!sve)} 
+                  buttonText={'ruotsi'} />
 
           <h3>Koulutustaso</h3>
-          <Button class={peruskoulu ? 'aktiivinen' : 'passiivinen'} suodatin={() => setPeruskoulu(!peruskoulu)} buttonText={'peruskoulu'} />
-          <Button class={ammattikoulu ? 'aktiivinen' : 'passiivinen'} suodatin={() => setAmmattikoulu(!ammattikoulu)} buttonText={'ammattikoulu'} />
-          <Button class={lukio ? 'aktiivinen' : 'passiivinen'} suodatin={() => setLukio(!lukio)} buttonText={'lukio'} />
-          <Button class={korkeakoulu ? 'aktiivinen' : 'passiivinen'} suodatin={() => setKorkeakoulu(!korkeakoulu)} buttonText={'korkeakoulu'} />
+          <Button class={peruskoulu ? 'active' : 'passive'} 
+                  filter={() => setPeruskoulu(!peruskoulu)} 
+                  buttonText={'peruskoulu'} />
+          <Button class={ammattikoulu ? 'active' : 'passive'} 
+                  filter={() => setAmmattikoulu(!ammattikoulu)} 
+                  buttonText={'ammattikoulu'} />
+          <Button class={lukio ? 'active' : 'passive'} 
+                  filter={() => setLukio(!lukio)} 
+                  buttonText={'lukio'} />
+          <Button class={korkeakoulu ? 'active' : 'passive'} 
+                  filter={() => setKorkeakoulu(!korkeakoulu)} 
+                  buttonText={'korkeakoulu'} />
 
           <h3>Etu- tai sukunimi</h3>
-          <Filter onTextChange={text => setFilterString(text)}/>
+          <Filter onTextChange={text => setCandidateName(text)}/>
+          
+          <small>Vastausvaihtoehdot perustuvat kansanedustajien tietoihin. Esimerkiksi muita kieliä ei valinnoissa ole, koska edustajista kaikki ovat suomen- tai ruotsinkielisiä tai tieto puuttuu.</small>
 
           <h3>- - -</h3>
-          <Button class="resetBtn" suodatin={reset()} buttonText="Poista kaikki valintasi"/>
+          <Button class="resetButton" 
+                  filter={reset()} 
+                  buttonText="Tyhjennä kaikki valintasi"/>
 
         </div>
-        <div className="tulokset">
+
+        <div id="tulos" className="results">
+
           <div className="kaltaisesi">
-            <h2>Kaltaisesi edustajat</h2>
-            <p className="yhteensä">Yhteensä <span className="invert">{filteredCandidates.length}</span> kansanedustajaa</p>
+            <h2>Sinua muistuttavia kansanedustajia ovat:</h2>
+            <p className="total">Yhteensä <span className="invert">{filteredCandidates.length}</span> kansanedustajaa</p>
+
             <ul>
               {filteredCandidates.map(d => {
                 if (d.memberOfParliament === false) {
                   return (
-                    <li className="uusi" key={Math.random(21)}><b>{d.firstName} {d.lastName}</b>, {d.age}, {d.abbr}<br/><span className="ammatti">{d.occupation}</span></li>
+                    <li className="uusi" key={Math.random(21)}>
+                        <b>{d.firstName} {d.lastName}</b>, {d.age}, {d.abbr}<br/>
+                        <span className="occupation">{d.occupation}</span>
+                    </li>
                   )
                 }
                 else {
                   return (
-                    <li key={Math.random(42)}><b>{d.firstName} {d.lastName}</b>, {d.age}, {d.abbr}<br/><span className="ammatti">{d.occupation}</span></li>
+                    <li key={Math.random(42)}>
+                      <b>{d.firstName} {d.lastName}</b>, {d.age}, {d.abbr}<br/>
+                      <span className="occupation">{d.occupation}</span>
+                    </li>
                   )
                 }
               } 
               )}
             </ul>
+
           </div>
         </div>
       </div>
@@ -144,7 +179,7 @@ const Filter = (props) => {
 const Button = (props) => {
   return (
     <>
-    <button className={props.class} onClick={props.suodatin}>{props.buttonText}</button>
+    <button className={props.class} onClick={props.filter}>{props.buttonText}</button>
     </>
   )
 }
